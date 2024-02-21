6bd5ac903b3de082363e6f638840f5cd86b441f3
Azalurg <91213389+Azalurg@users.noreply.github.com>
2024-02-21 12:38:23 +0100

    STC-102: Add simple users_list view

--- stockApp/views.py
+++ stockApp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from stockApp.models import CustomUser
+from stockApp.serializers import UserSerializer
+
+
+@csrf_exempt
+def users_list(request):
+    if request.method == 'GET':
+        snippets = CustomUser.objects.all()
+        serializer = UserSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
