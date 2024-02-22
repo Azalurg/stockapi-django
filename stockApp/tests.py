3ca58099edb28e300c91eaedf224c212cd59a1c4
Azalurg <91213389+Azalurg@users.noreply.github.com>
2024-02-22 11:56:22 +0100

    STC-102: Add single user tests - GET user by id - Create a user factory

--- stockApp/tests.py
+++ stockApp/tests.py
@@ -1,13 +1,18 @@
+import factory.django
 from django.test import Client, TestCase
 
 from stockApp.models import CustomUser
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "stockApp.CustomUser"
 
 
 class TestUsersListEndpoint(TestCase):
     def setUp(self):
         self.c = Client()
         CustomUser.objects.create_superuser(email="admin@example.com", first_name="admin", last_name="admin", password="admin")
-        CustomUser.objects.create_superuser(email="admin2@example.com", first_name="admin", last_name="admin", password="admin")
 
     def test_get_users_list(self):
         response = self.c.get("/users/")
@@ -25,3 +30,11 @@ class TestUsersListEndpoint(TestCase):
 
         self.assertEqual(response.status_code, 201)
         self.assertEquals(response.data["first_name"], "Harry")
+
+    def test_get_user_by_id(self):
+        user: CustomUser = UserFactory.create()
+
+        response = self.c.get(f"/users/{user.pk}")
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.data["id"], user.pk)
