3e742fdcdcf169433c03942e88e5b65050c3b47d
Azalurg <91213389+Azalurg@users.noreply.github.com>
2024-02-22 13:48:07 +0100

    STC-102: Make some user fields read-only

--- stockApp/serializers.py
+++ stockApp/serializers.py
@@ -5,4 +5,5 @@ from stockApp.models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["id", "email", "first_name", "last_name", "created_at"]
+        fields = ["id", "email", "first_name", "last_name", "created_at", "modified_at"]
+        read_only_fields = ["id", "created_at", "modified_at"]
