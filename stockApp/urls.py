7a04fd9d8270d86ef9cc845336c068ea1234d6da
Azalurg <91213389+Azalurg@users.noreply.github.com>
2024-02-22 14:43:15 +0100

    STC-103: Setup Simple JWT

--- stockApp/urls.py
+++ stockApp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from stockApp import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('', views.UsersList.as_view()),
-    path('<int:pk>', views.UsersDetail.as_view())
+    path('<int:pk>', views.UsersDetail.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
--- stockProject/settings.py
+++ stockProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'stockApp',
 ]
 
@@ -129,3 +130,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "stockApp.CustomUser"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
