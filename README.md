457b94b1bcc1d4abefbd39538c4a52b5ef15cbf6
Azalurg <91213389+Azalurg@users.noreply.github.com>
2024-02-28 14:46:24 +0100

    STC-106: Add example Celery configuration

--- README.md
+++ README.md
@@ -17,3 +17,27 @@ To run the application execute command:
 ```
 python manage.py runserver
 ```
+
+## Celery
+
+- Run broker RabbitMQ (example with docker)
+
+```sh
+docker run -d -p 5672:5672 rabbitmq
+```
+
+- Run Celery worker and beat
+
+```sh
+celery -A stockProject worker -l info
+```
+
+```sh
+celery -A stockProject beat -l info
+```
+
+- Run Flower monitor
+
+```sh
+celery --broker=amqp://guest:guest@localhost:5672// flower
+```
--- stockProject/settings.py
+++ stockProject/settings.py
@@ -15,6 +15,8 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+from stockProject.tasks import add
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_results',
     'rest_framework',
     'rest_framework_simplejwt',
     'stockApp',
@@ -140,4 +143,17 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=int(env("ACCESS_TOKEN_LIFETIME"))),
+}
+
+# Celery
+
+CELERY_RESULT_BACKEND = "django-db"
+CELERY_TIMEZONE = 'Europe/Warsaw'
+
+CELERY_BEAT_SCHEDULE = {
+    'test-task': {
+        'task': "stockProject.tasks.add",
+        'schedule': 5.0,
+        'args': (1, 2)
+    },
 }
